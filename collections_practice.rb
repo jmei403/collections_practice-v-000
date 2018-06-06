9e8ec69eb42e2763f2aa7921f317e2f34fb79d56
Jason Mei <jason.mei403@gmail.com>
2018-06-06 03:00:19 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,10 +2,10 @@ def sort_array_asc(array)
   array.sort
 end
 
-def sort_array_desc(array)
-  array.sort do |a, b|
-
-end
+# def sort_array_desc(array)
+#   array.sort do |a, b|
+#
+# end
 
 def reverse_array(array)
   array.sort do |a, b|
